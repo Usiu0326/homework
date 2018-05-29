cfd30effa042c21280a0cacf115aa100952ff3a8
usiu <usiu0326@gmail.com>
2018-05-29 14:04:48 +0800

    指向非常數資料的非常數指標

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#define SIZE 10
+
+void bubbleSort( int  *  array, int size);    
+ 
+int main(void)
+{
+   int a[10]={5,7,21,39,14,99,11,46,20,16};
+   int i,size;
+   
+   puts("資料排序前");
+   
+    for(i=0;i<=9;i++)
+    {
+     printf("%d ",a[i]);
+    }
+    bubbleSort( a, 10);
+    
+ puts("\n資料排序後\n");
+ for(i=0;i<=9;i++)
+ {
+  printf("%d ",a[i]);
+ }
+}
+
+void swap(int *element1Ptr,int *element2Ptr)
+{
+ int hold= *element1Ptr;
+ *element1Ptr=*element2Ptr;
+ *element2Ptr=hold;
+}
